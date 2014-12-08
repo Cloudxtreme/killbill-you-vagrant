8b33116dd565132db8fad3f56b36f1da8aeced5a
Andrew Varnerin <andrew@varnerin.info>
2014-12-07 19:02:08 -0500

    install killbill dbs on provision

--- custom_cookbooks/killbill-setup/recipes/default.rb
+++ custom_cookbooks/killbill-setup/recipes/default.rb
@@ -1,5 +1,7 @@
 include_recipe "database::mysql"
 
+# DB junk
+
 mysql_connection_info = {:host => "localhost",
                          :username => 'root',
                          :password => node['mysql']['server_root_password']}
@@ -20,4 +22,15 @@ mysql_database_user 'killbill' do
 	database_name 'killbill'
 	password 'killbill'
 	action :grant
+end
+
+# Install killbill DBs
+
+bash "install_killbill_dbs" do
+	user "root"
+	cwd "/tmp"
+	code <<-EOH
+	curl http://killbill.io/wp-content/uploads/2014/11/killbill-0.12.0.ddl | mysql -h 127.0.0.1 -ukillbill -pkillbill killbill
+	echo Installed killbill tables
+	EOH
 end
